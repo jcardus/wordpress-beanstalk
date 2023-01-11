bc297ee637107f146546a866c494351b0d151a64
jcardus <asklocation.net@gmail.com>
2023-01-11 10:35:35 +0000

    Enabling the Network

--- wp-config.php
+++ wp-config.php
@@ -14,6 +14,12 @@ define('SECURE_AUTH_SALT', $_SERVER['SECURE_AUTH_SALT']);
 define('LOGGED_IN_SALT',   $_SERVER['LOGGED_IN_SALT']);
 define('NONCE_SALT',       $_SERVER['NONCE_SALT']);
 define('WP_ALLOW_MULTISITE', true );
+define( 'MULTISITE', true );
+define( 'SUBDOMAIN_INSTALL', true );
+define( 'DOMAIN_CURRENT_SITE', 'wordpress-env.eba-kdv6e7km.eu-west-3.elasticbeanstalk.com' );
+define( 'PATH_CURRENT_SITE', '/' );
+define( 'SITE_ID_CURRENT_SITE', 1 );
+define( 'BLOG_ID_CURRENT_SITE', 1 );
 $table_prefix  = 'wp_';
 define('WP_DEBUG', false);
 if ( !defined('ABSPATH') )
